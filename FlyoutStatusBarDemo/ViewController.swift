7c322b0513d0c127d5959cffb237b9d459715e9d
iblagajic <iblagajic@me.com>
2016-05-31 14:16:17 +0200

    Initial Commit

---
+++ FlyoutStatusBarDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlyoutStatusBarDemo
+//
+//  Created by Ivan Blagajić on 31/05/16.
+//  Copyright © 2016 Ivan Blagajić. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
